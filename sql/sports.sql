3c1ed5010f097fc5fce5a1e48030963f6bf902be
mhou <rambo@mobplus.net>
2020-12-10 16:26:15 +0800

    项目初始化

--- sql/sports.sql
+++ sql/sports.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50627
 File Encoding         : 65001
 
-Date: 2020-12-08 13:58:08
+Date: 2020-12-10 15:51:42
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,11 +30,16 @@ CREATE TABLE `advisory` (
   `gmt_create` timestamp NOT NULL DEFAULT '1970-12-31 16:00:00' COMMENT '创建时间',
   `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of advisory
 -- ----------------------------
+INSERT INTO `advisory` VALUES ('1', 'dasd', '<p>asdasd</p>', 'dasd', '1', null, 'dasd', '2020-12-08 18:24:05', '2020-12-08 18:24:05');
+INSERT INTO `advisory` VALUES ('5', 'dsad', '<p>dsad</p>', 'sadsa', '2', null, 'dasdas', '2020-12-08 18:28:29', '2020-12-08 18:28:29');
+INSERT INTO `advisory` VALUES ('6', 'dsad', '<p>dsasd</p>', 'sadas', '3', null, 'dasd', '2020-12-08 18:28:35', '2020-12-08 18:28:35');
+INSERT INTO `advisory` VALUES ('7', 'dsad', '<p>dasd</p>', 'asdas', '5', null, 'dasd', '2020-12-08 18:28:42', '2020-12-08 18:28:42');
+INSERT INTO `advisory` VALUES ('8', 'dsa', '<p><br></p>', 'dsa', '5', null, 'dsad', '2020-12-08 18:31:21', '2020-12-08 18:31:21');
 
 -- ----------------------------
 -- Table structure for announcement
@@ -50,11 +55,12 @@ CREATE TABLE `announcement` (
   `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `image` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of announcement
 -- ----------------------------
+INSERT INTO `announcement` VALUES ('2', 'dasd', 'dsa', null, '<p>dsad</p>', '2020-12-10 15:50:38', '2020-12-10 15:50:38', null);
 
 -- ----------------------------
 -- Table structure for comment
@@ -68,6 +74,7 @@ CREATE TABLE `comment` (
   `apply` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `flag` int(11) DEFAULT NULL,
   `aboy` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
   `gmt_create` timestamp NOT NULL DEFAULT '1970-12-31 16:00:00' COMMENT '创建时间',
   `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -130,6 +137,7 @@ CREATE TABLE `message` (
   `flag` int(11) DEFAULT NULL,
   `title` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
   `checkboy` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
   `gmt_create` timestamp NOT NULL DEFAULT '1970-12-31 16:00:00' COMMENT '创建时间',
   `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -164,8 +172,8 @@ CREATE TABLE `sysuser` (
 -- ----------------------------
 -- Records of sysuser
 -- ----------------------------
-INSERT INTO `sysuser` VALUES ('2', 'cmh', '4c41c8ee9e59e72c0463f87ffb7d356f', 'mj0x89ImC/rRoC4yK7bZGg==', '1', '15362478021', '1', '/images/now-logo.jpg', '男', '106358@qq.com', '2020-12-04 11:45:34', '2020-12-04 17:55:12', '201653684', '陈明浩');
-INSERT INTO `sysuser` VALUES ('3', 'rzh', '6373b90b4be5aa7ddcb0843265bc57a9', 'RwgAOcVSZF/TiZifb5UJgw==', '0', '15236984723', '1', '/images/now-logo.jpg', '男', '1536982@qq.com', '2020-12-04 18:07:00', '2020-12-04 18:07:00', '201758964', '容志恒');
+INSERT INTO `sysuser` VALUES ('2', 'cmh', '4c41c8ee9e59e72c0463f87ffb7d356f', 'mj0x89ImC/rRoC4yK7bZGg==', '1', '15362478021', '1', '/images/noimage.jpg', '男', '106358@qq.com', '2020-12-04 11:45:34', '2020-12-04 17:55:12', '201653684', '陈明浩');
+INSERT INTO `sysuser` VALUES ('3', 'rzh', '6373b90b4be5aa7ddcb0843265bc57a9', 'RwgAOcVSZF/TiZifb5UJgw==', '0', '15236984723', '1', '/images/noimage.jpg', '男', '1536982@qq.com', '2020-12-04 18:07:00', '2020-12-04 18:07:00', '201758964', '容志恒');
 
 -- ----------------------------
 -- Table structure for team
